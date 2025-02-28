a429ed5de37fca73df0c9e571ccf221d1b841770
Vijay Mohan <vijay1979@msn.com>
2025-02-28 13:07:42 -0500

    Enhance console output in Performance Test app
    Added informative messages to highlight the benefits of using redfly.ai technology, including features like Redis synchronization, automatic backend code generation, and gRPC hosting. Included a prompt for users to start the performance test.

--- RedflyPerformanceTest/Program.cs
+++ RedflyPerformanceTest/Program.cs
@@ -12,6 +12,14 @@ namespace RedflyPerformanceTest
             {
                 Console.Title = "redfly.ai - Performance Test Console";
 
+                Console.WriteLine("This console app is intended to demonstrate the benefits of using backend services built with redfly.ai tech over conventional data access techniques.\r\n");
+
+                Console.WriteLine("1. We natively sync ANY database schema with Redis in the background.");
+                Console.WriteLine("2. We can generate the backend code for any database with Redis caching built-in.");
+                Console.WriteLine("3. This is hosted over Grpc.");
+
+                Console.WriteLine("\r\n Now imagine you being able to do that with your database, without any manual effort! That's what our product does. Contact us at developer@redfly.ai to directly work with us so you can do the same thing with your database (cloud/ on-premises) - no matter how large or complex it is, redfly.ai can do it! \r\n");
+
                 Console.WriteLine("Press any key to start the performance test...");
                 Console.ReadKey();
 
