92cbe19835686eb0bcd717d1cbc994566de258c1
Vijay Mohan <vijay1979@msn.com>
2025-03-07 21:45:48 -0500

    Add RedflyDatabaseSyncProxy project and console app
    This commit introduces a new project named "RedflyDatabaseSyncProxy" to the solution. It includes a `Program.cs` file with an asynchronous `Main` method for a console application that demonstrates synchronization services with Redis. The application provides user instructions and handles gRPC authentication errors. Additionally, the project file `RedflyDatabaseSyncProxy.csproj` has been created, detailing project settings, dependencies, and a reference to the `RedflyGrpcAuthServiceClient` project.

---
+++ RedflyDatabaseSyncProxy/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace RedflyDatabaseSyncProxy;
+
+internal class Program
+{
+    internal static async Task Main(string[] args)
+    {
+        try
+        {
+            Console.Title = "redfly.ai - Database Sync Proxy";
+
+            Console.WriteLine("This console app is intended to allow anyone to try out our synchronization services on-demand.\r\n");
+
+            Console.WriteLine("1. We natively sync ANY database schema with Redis in the background.");
+            Console.WriteLine("2. We can generate the backend code for any database with Redis caching built-in.");
+            Console.WriteLine("3. This is hosted over Grpc.");
+
+            Console.WriteLine("\r\nNow imagine you being able to do that with your database, without any manual effort! That's what our product does.");
+            Console.WriteLine("Contact us at developer@redfly.ai to directly work with us so you can do the same thing with your database (cloud/ on-premises).");
+            Console.WriteLine("No matter how large or complex your DB is, redfly.ai can do it! \r\n");
+
+            Console.WriteLine("Press any key to start the performance test...");
+            Console.ReadKey();
+
+            //TODO: This will change.
+            var grpcUrl = "https://localhost:7176";
+
+            var grpcAuthToken = await RedflyGrpcAuthServiceClient.AuthGrpcClient.RunAsync(grpcUrl);
+
+            if (grpcAuthToken == null ||
+                grpcAuthToken.Length == 0)
+            {
+                Console.WriteLine("Failed to authenticate with the gRPC server.");
+                Console.WriteLine("Contact us at developer@redfly.ai if you need to.");
+                return;
+            }
+
+            //TBD
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            Console.WriteLine("Contact us at developer@redfly.ai if you need to.");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
