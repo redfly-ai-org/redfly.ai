1c51e29816fc971aa18ec3393a39db215fe76dbe
Vijay Mohan <vijay1979@msn.com>
2025-05-09 16:41:25 -0400

    Improve console output formatting in MongoReady.cs
    Added newline characters to various Console.WriteLine statements
    to enhance readability. This change improves the formatting of
    instructions for setting up MongoDB Change Streams, user
    permissions, enabling Change Streams, and increasing the Oplog size.

--- RedflyDatabaseSyncProxy/Databases/MongoReady.cs
+++ RedflyDatabaseSyncProxy/Databases/MongoReady.cs
@@ -51,7 +51,7 @@ namespace RedflyDatabaseSyncProxy
             do
             {
                 Console.WriteLine("1. Deploy a Replica Set\r\n");
-                Console.WriteLine("   MongoDB should be running as a replica set or in a sharded cluster");
+                Console.WriteLine("   MongoDB should be running as a replica set or in a sharded cluster\r\n");
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("   Stop the MongoDB server.");
                 Console.WriteLine("   Start the MongoDB server with the --replSet option: mongod --replSet \"rs0\"");
@@ -59,7 +59,7 @@ namespace RedflyDatabaseSyncProxy
                 Console.WriteLine("   mongo");
                 Console.WriteLine("   rs.initiate()");
                 Console.ResetColor();                
-                Console.WriteLine("   If you're using MongoDB Atlas, all clusters are deployed as replica sets by default\r\n");
+                Console.WriteLine("\r\n   If you're using MongoDB Atlas, all clusters are deployed as replica sets by default\r\n");
 
                 Console.WriteLine("Please enter 'y' when you are ready to continue AFTER ensuring this setup...");
                 response = Console.ReadLine();
@@ -70,7 +70,7 @@ namespace RedflyDatabaseSyncProxy
             do
             {
                 Console.WriteLine("\r\n2. Set Database User Permissions\r\n");
-                Console.WriteLine("   Assign the readWrite role to the user for the database you want to monitor.");
+                Console.WriteLine("   Assign the readWrite role to the user for the database you want to monitor.\r\n");
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("   db.createUser({");
                 Console.WriteLine("       user: \"changeStreamUser\",");
@@ -81,7 +81,7 @@ namespace RedflyDatabaseSyncProxy
                 Console.WriteLine("       ]");
                 Console.WriteLine("   });");
                 Console.ResetColor();
-                Console.WriteLine("   For monitoring all collections, you can also assign the read or readWrite role for the entire database.\r\n");
+                Console.WriteLine("\r\n   For monitoring all collections, you can also assign the read or readWrite role for the entire database.\r\n");
 
                 Console.WriteLine("Please enter 'y' when you are ready to continue AFTER modifying the user role...");
                 response = Console.ReadLine();
@@ -96,9 +96,9 @@ namespace RedflyDatabaseSyncProxy
                 Console.WriteLine("   Change streams are supported starting from MongoDB 3.6.");
                 Console.WriteLine("   Change streams are enabled by default for replica sets and sharded clusters.");
                 Console.ResetColor();                
-                Console.WriteLine("   Change Streams are enabled by default on MongoDB Atlas.");
+                Console.WriteLine("\r\n   Change Streams are enabled by default on MongoDB Atlas.");
                 Console.WriteLine("   If you're using MongoDB Atlas, the database should not be using a Free Tier Cluster (M0).");
-                Console.WriteLine("MongoDB Change Streams require at least an M10 or higher cluster.\r\n");
+                Console.WriteLine("   MongoDB Change Streams require at least an M10 or higher cluster.\r\n");
 
                 Console.WriteLine("Please enter 'y' when you are ready to continue AFTER enabling change streams on the database...");
                 response = Console.ReadLine();
@@ -112,7 +112,7 @@ namespace RedflyDatabaseSyncProxy
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("   Increase the oplog size to ensure that the oplog doesn't roll over before your application processes changes.");
                 Console.ResetColor();
-                Console.WriteLine("   Change Streams rely on the oplog (operations log).");
+                Console.WriteLine("\r\n   Change Streams rely on the oplog (operations log).");
                 Console.WriteLine("   This is necessary IF your database has a high write throughput.\r\n");
 
                 Console.WriteLine("Please enter 'y' when you are ready to continue AFTER increasing the Oplog size (if found necessary)...");
