e5b88e49f41eace6f397732df5ed5ccdfeb3db20
Vijay Mohan <vijay1979@msn.com>
2025-05-18 19:13:33 -0400

    Add QueryResult class for flexible data storage
    Introduces a new `QueryResult` class in the `redflyDataAccessClient` namespace.
    This class features a `Rows` property, which is a list of dictionaries mapping strings to objects.
    Includes necessary using directives for collections and other functionalities.

---
+++ redflyDataAccessClient/QueryResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redflyDataAccessClient;
+
+public class QueryResult
+{
+    public List<Dictionary<string, object>> Rows { get; set; } = new();
+}
